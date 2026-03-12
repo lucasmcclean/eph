11c6d3555ffa25cbeca28909792e2da8c7c4f38d
Lucas McClean <lucksdeveloper@gmail.com>
2026-03-11 20:39:13 -0400

    feat(edit): complete command

--- src/commands/edit.rs
+++ src/commands/edit.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
+use uuid::Uuid;
 
-use crate::task::Priority;
+use crate::{storage, task::Priority};
 
 #[derive(Clone, Debug, Parser)]
 #[command(name = "eph")]
@@ -11,26 +12,24 @@ use crate::task::Priority;
             .required(true)
             .args([
                 "title",
-                "to_context",
+                "context",
                 "priority",
                 "add_tags",
                 "delete_tags",
                 "new_tags",
                 "description",
+                "delete_description",
             ])
     )
 )]
 pub struct Edit {
-    identifier: String,
-
-    #[arg(short, long)]
-    in_context: Option<String>,
+    id: Uuid,
 
     #[arg(short, long)]
     title: Option<String>,
 
-    #[arg(short = 'c', long)]
-    to_context: Option<String>,
+    #[arg(short, long)]
+    context: Option<String>,
 
     #[arg(short, long, help = "Priority: one of high, medium, low, backlog")]
     priority: Option<Priority>,
@@ -46,10 +45,55 @@ pub struct Edit {
 
     #[arg(short = 'e', long)]
     description: Option<String>,
+
+    #[arg(
+        short = 'r',
+        long,
+        value_delimiter = ',',
+        conflicts_with = "description"
+    )]
+    delete_description: bool,
 }
 
 impl Edit {
     pub fn run(&self) {
-        println!("edit");
+        let mut tasks = storage::load(storage::DataPath::default()).unwrap();
+
+        let edited_task = tasks.iter_mut().find(|task| task.id() == self.id);
+
+        match edited_task {
+            Some(task) => {
+                if let Some(title) = &self.title {
+                    task.title = title.clone();
+                }
+
+                if let Some(context) = &self.context {
+                    task.context = context.clone();
+                }
+
+                if let Some(priority) = &self.priority {
+                    task.priority = *priority;
+                }
+
+                if let Some(description) = &self.description {
+                    task.description = Some(description.clone());
+                }
+
+                if self.delete_description {
+                    task.description = None;
+                }
+
+                if !self.new_tags.is_empty() {
+                    task.tags = self.new_tags.clone();
+                } else {
+                    task.tags.extend(self.add_tags.iter().cloned());
+
+                    task.tags.retain(|tag| !self.delete_tags.contains(tag));
+                }
+                storage::store(storage::DataPath::default(), &tasks).expect("Failed to save tasks");
+                println!("Successfully edited");
+            }
+            None => println!("Item not found"),
+        }
     }
 }
